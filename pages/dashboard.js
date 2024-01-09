580fd693573bbabf0737d5dc3f8a24c87a615120
Udhaya Kumar D <udhayakumar.d@idp.com>
2024-01-09 14:03:59 +0530

    changes made in auth adn other files

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -3,11 +3,10 @@ import AdminBoard from "@/components/Dashboard/AdminBoard";
 import { useSelector, useDispatch } from "react-redux";
 import { useRouter } from "next/router";
 const dashboard = () => {
-  return (
-    <div>
-      <AdminBoard />
-    </div>
-  );
+  const router = useRouter();
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
+  return <div>{isLoggedIn && <AdminBoard />}</div>;
 };
 
 export default dashboard;
