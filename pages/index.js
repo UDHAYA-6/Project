3fb0c1cba50c17c704142d5a09ca47362c892e10
Udhaya kumar <udhaya642003@gmail.com>
2024-01-30 21:22:15 +0530

    mobile restricted

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import NavBar from "@/components/Genral NavBar/nav";
 import Bus from "@/components/Bus and Bus Search/Bus";
 import BusSearch from "@/components/Bus and Bus Search/BusSearch";
+import { isMobile } from "react-device-detect";
 
 const Page = () => {
   const [data, setData] = useState([]);
@@ -17,12 +18,30 @@ const Page = () => {
       window.scrollTo({ top: 700, behavior: "smooth" });
     }
   }, [data]);
-
   return (
     <>
-      <NavBar />
-      <BusSearch getData={handleData} getDate={handleDate} />
-      {data.length > 0 && <Bus values={data} date={Date} />}
+      {!isMobile ? (
+        <>
+          <NavBar />
+          <BusSearch getData={handleData} getDate={handleDate} />
+          {data.length > 0 && <Bus values={data} date={Date} />}
+        </>
+      ) : (
+        <center>
+          <div
+            style={{
+              textAlign: "center",
+              padding: "20px",
+              backgroundColor: "#FFEB3B",
+            }}
+          >
+            <p>
+              This application is optimized for PC and desktop. Please use a
+              computer for the best experience.
+            </p>
+          </div>
+        </center>
+      )}
     </>
   );
 };
